a5a4e7df6237f58527bfb179a5d59988eee053c3
Briaares <briares2698@gmail.com>
2015-09-22 23:21:31 -0500

    Update Pulseras.cpp

--- Etapa-1---Problemset-11/Pulseras.cpp
+++ Etapa-1---Problemset-11/Pulseras.cpp
@@ -1,17 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000007
-#define MAX 1000002
-long long v[2][MAX],n;
-int solve() {
-	v[0][0]=1;
-   for(int i=1;i<n;i++){ 
-   v[0][i]=(v[0][i-1]+v[1][i-1])%MOD; 
-   v[1][i]=v[0][i-1];
+#define MOD %1000000007
+#define MAX 1000009
+#define llu unsigned long long 
+llu v[MAX],n;
+llu solve() {
+	v[0]=v[1]=1;
+   for(llu i=2;i<n+2;i++){ 
+   v[i]=v[i-1]+v[i-2]MOD; 
    }
-   return (v[0][n-1]+(v[1][n-1]*2)%MOD)%MOD;
+   return (v[n]+v[n-2])MOD;
 }
-int main(){
+int main(){ //Esto es Fibonnaci
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
     cin>>n;
