82d934a1b70647d3fb0c03543de465be84a1f7d8
Briaares <briares2698@gmail.com>
2015-12-27 01:43:39 -0600

    Update the knapsack problem.cpp

--- Guanajuato/the knapsack problem.cpp
+++ Guanajuato/the knapsack problem.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
-#define MAX 10002
-int dp[MAX][MAX],p[MAX],u[MAX],d,n;
 using namespace std;
-int solve(int i,int m){
-	if(m<0||i==n)return 0;
-	if(dp[i][m])return dp[i][m];
-	if(m-p[i]>=0)dp[i][m]=max(solve(i+1,m-p[i])+u[i],solve(i+1,m));
-	return dp[i][m];
+#define MAX 502
+#define MAXN 10002
+int u[MAX],p[MAX],memo[MAX][MAXN],n;
+int dp(int i,int m){
+	if(i==n||m==0)return 0;
+	if(m<0)return -(1<<30);
+	if(memo[i][m])return memo[i][m];
+	memo[i][m]=max(dp(i+1,m-p[i])+u[i],dp(i+1,m));
+	return memo[i][m];
 }
-int main(){
-    ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	int i,k=0;
-	cin>>d>>n;
-	for(i=0;i<n;i++){
-		cin>>p[i]>>u[i];
+	int main (){
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		int d,i,j,k=0;
+		cin>>d>>n;
+		for(i=0;i<n;i++){
+			cin>>p[i]>>u[i];
+		}
+		cout <<dp(0,d)<<"\n";
 	}
-	for(i=0;i<n;i++){
-		k=max(k,solve(i,d));
-	}
-	cout << k<<"\n";
-}
