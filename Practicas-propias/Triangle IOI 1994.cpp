2c13f89cba278f152d5638293f119156db13ab4d
Briaares <briares2698@gmail.com>
2016-01-04 19:48:32 -0600

    OMEGAUP

---
+++ Practicas-propias/Triangle IOI 1994.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 102
+int m[MAX][MAX],memo[MAX][MAX],n;
+int dp(int i,int j){
+	if(i==n||m[i][j]==-1)return 0;
+	if(memo[i][j])return memo[i][j];
+	return memo[i][j]=m[i][j]+max(dp(i+1,j),dp(i+1,j+1));
+}
+	int main (){
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+			memset(m,-1,sizeof(m));
+			cin>>n;
+			for(int i=0;i<n;i++){
+				for(int j=0;j<=i;j++){
+					cin>>m[i][j];
+				}
+			}
+			cout << dp(0,0)<<"\n";	
+	}
