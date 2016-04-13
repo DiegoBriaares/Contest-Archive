d04ab4ad27837c2c94efb18814da2bb42505acb0
Briaares <briares2698@gmail.com>
2016-04-13 12:07:41 -0500

    OMEGAUP

--- Practicas-propias/L-OMI2010-Espias.cpp
+++ Practicas-propias/L-OMI2010-Espias.cpp
@@ -1,50 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 1000003
-int v[MAX],memo[MAX],memito[MAX],pila[MAX],r[MAX];
-int in=1,n;
+int n,v[MAX],memo[MAX],res=0,r[MAX],pila[MAX],in=1;
 	int main (){
 		ios_base::sync_with_stdio(0);
 		cin.tie(0);
 		cin>>n;
-		int i,j,k,g,ciclo=1;
-		for(i=1;i<=n;i++){
-			cin>>v[i];
-		}
-		for(i=1;i<=n;i++){
-			if(!memito[i]){
-				for(j=i,k=0;!memito[j];){
-					memito[j]=ciclo;
+		int j;
+		for(int i=1;i<=n;i++)cin>>v[i];
+		for(int i=1;i<=n;i++){
+			if(!memo[i]){
+				for(j=i;!memo[j];){
+					memo[j]=i;
 					pila[in++]=j;
 					j=v[j];
 				}
-				if(memito[j]==ciclo){
-					k=1;
-				for(g=in-1;g>=1;g--){
-					if(pila[g]==j)break;
-					else k++;
-				}
-				bool incicle=true;
-				while(in>1){
-					in--;
-					if(incicle)r[pila[in]]=k;
-					else {
-						r[pila[in]]=r[v[pila[in]]]+1;
+				if(memo[j]==i){
+					int uso=1,cicle=1;
+					for(int g=in-1;g>=1;g--){
+						if(pila[g]==j)break;
+						uso++;
 					}
-					if(pila[in]==j)incicle=false;
-				}
+					while(in>=1){
+						in--;
+						if(cicle)r[pila[in]]=uso;
+						else r[pila[in]]=r[v[pila[in]]]+1;
+						res=max(res,r[pila[in]]);
+						if(pila[in]==j)cicle=0;
+					}
 				}
 				else {
-					while(in>1){
+					while(in>=1){
 						in--;
 						r[pila[in]]=r[v[pila[in]]]+1;
+						res=max(res,r[pila[in]]);
 					}
 				}
 			}
-			ciclo++;
 		}
-		k=0;
-		//for(i=1;i<=n;i++)cout <<r[i]<<" ";
-		for(i=1;i<=n;i++)k=max(k,r[i]);
-		cout <<k<<"\n";
+		cout << res<<"\n";
 	}
