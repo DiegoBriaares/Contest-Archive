715e57ab4d59753978be50d1bdabcf73a173be2c
Briaares <briares2698@gmail.com>
2016-04-20 12:09:13 -0500

    OMEGAUP

--- Practicas-propias/Formando números.cpp
+++ Practicas-propias/Formando números.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define MOD 1000000009
+#define MOD 1000000000
 #define ll long long
-ll n,k,dp[1000][9000];
+int n,k,dp[1003][9003];
 	int main (){
 		ios_base::sync_with_stdio(0);
 		cin.tie(0);
@@ -12,7 +12,8 @@ ll n,k,dp[1000][9000];
 		for(i=1;i<=n;i++){
 			for(j=0;j<=k;j++){
 				for(uso=0;uso<=9&&j-uso>=0;uso++){
-					dp[i][j]+=dp[i-1][j-uso]%MOD;
+					dp[i][j]+=(dp[i-1][j-uso]);
+					dp[i][j]%=MOD;
 				}
 			}
 		}
