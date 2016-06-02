c00a896b68a791347ffb98a9782e5b99a5763eb1
Diego González Hernández <briares2698@gmail.com>
2016-06-02 11:13:05 -0500

    OMEGAUP

---
+++ Practicas-propias/El subterráneo camino de Shamash.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll n;
+stack<string>year;
+void add(int x){
+	string v="";
+	int uso=(x%10);
+	x/=10;
+	while(uso>0){
+		v+="I";
+		uso--;
+	}
+	uso=(x%10);
+	while(uso>0){
+		v+="L";
+		uso--;
+	}
+	reverse(v.begin(),v.end());
+	year.push(v);
+}
+	int main (){
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cin>>n;
+		add(n%60);
+		n/=60;
+		while(n>0){
+			year.push(".");
+			add(n%60);
+			n/=60;
+		}
+		while(!year.empty()){
+			cout <<year.top();
+			year.pop();
+		}
+	}
