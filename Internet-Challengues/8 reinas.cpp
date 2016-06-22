2764f993c64bc56963467033e5c988e76df93bbd
Diego González Hernández <briares2698@gmail.com>
2016-06-22 17:09:49 -0500

    Backtracking

--- Internet-Challengues/8 reinas.cpp
+++ Internet-Challengues/8 reinas.cpp
@@ -1,74 +1,62 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,a,b;
+#define MAX 10 
+bool m[MAX][MAX];
+bool r[MAX][MAX];
+bool v[MAX];
 bool ok=false;
-bool m[9][9];
-bool danger(int v[],int j,int i){
-	memset(m,false,sizeof(m));
-	for(int k=1;k<=8;k++){
-		if(k!=j)m[v[k]][k]=true;
-	}
-	for(int k=i;k>0;k--){   //Vertical abajo
-		if(m[k][j])return false;
-	}
-	for(int k=i;k<9;k++){	//Vertical arriba
-		if(m[k][j])return false;
-	}
-	for(int k=j;k<9;k++){	//Horizontal derecha
-		if(m[i][k])return false;
-	}
-	for(int k=j;k>0;k--){	//Horizontal izquierda
-		if(m[i][k])return false;
-	}
-	for(int k=i,k2=j;k<9&&k2<9;k++,k2++){	//Diagonal abajo derecha
-		if(m[k][k2])return false;
-	}
-	for(int k=i,k2=j;k>0&&k2<9;k--,k2++){	//Diagonal arriba derecha
-		if(m[k][k2])return false;
-	}
-	for(int k=i,k2=j;k<9&&k2>0;k++,k2--){	//Diagonal abajo izquierda
-		if(m[k][k2])return false;
-	}
-	for(int k=i,k2=j;k>0&&k2>0;k--,k2--){	//Diagonal arriba izquierda
-		if(m[k][k2])return false;
-	}
-	return true;
+void res(){
+	for(int i=1;i<=8;i++){
+			for(int j=1;j<=8;j++){
+				if(r[i][j])cout << i<<" "<<j<<"\n";
+			}
+		}
 }
-void solve(int i,int v[]){
-	if(ok)return;
+bool check(int a,int b){
+	if(ok)return false;
+		for(int i=1;i<=8;i++){
+			for(int j=1;j<=8;j++){
+				if(m[i][j]&&i==a||m[i][j]&&j==b||m[i][j]&&(a-b)==(i-j)||m[i][j]&&(a+b)==(i+j))return false;
+			}
+		}
+		return true;
+	}
+bool solve(int i){
 	if(i==9){
 		ok=true;
-		for(int j=1;j<=8;j++){
-			cout << v[j]<<" "<<j<<"\n";
-		}
-		return;
+		return true;
 	}
 	if(v[i]){
-		if(danger(v,i,v[i]))solve(i+1,v);
-		return;
+		for(int j=1;j<=8;j++){
+			if(check(i,j))return false;
+		}
+		return solve(i+1);
 	}
 	for(int j=1;j<=8;j++){
-		if(danger(v,i,j)){
-			v[i]=j;
-			solve(i+1,v);
-			v[i]=0;
+		if(check(i,j)){
+			m[i][j]=true;
+			if(solve(i+1)){
+				v[i]=true;
+				r[i][j]=true;
+				return true;
+			}
+			m[i][j]=false;
 		}
 	}
+	return false;
 }
 	int main (){
 		ios_base::sync_with_stdio(0);
-		cin.tie(0);	
-		int v[9];
-		memset(v,0,sizeof(v));
-		cin>>n;
-		for(int i=0;i<n;i++){
+		cin.tie(0);
+		int t,a,b;
+		cin>>t;
+		while(t--){
 			cin>>a>>b;
-			if(danger(v,b,a))v[b]=a;
-			else {
-				cout << 0<<"\n";
-				return 0;
-			}
+			m[a][b]=true;
+			r[a][b]=true;
+			v[a]=true;
 		}
-		solve(1,v);
-		if(!ok)cout << 0<<"\n";
+		int uso=solve(1);
+		if(ok)res();
+		else cout <<0;
 	}
