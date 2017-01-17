6bfcff7f5d4f3b1f1e1719a443d35cc5f4e09db3
Diego González Hernández <diego.gonzalez@cimat.mx>
2017-01-16 22:46:04 -0600

    1

--- Etapa-1-Problemset-13/Karel Arcaico.cpp
+++ Etapa-1-Problemset-13/Karel Arcaico.cpp
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define MAX 102
+string v;
+int memo[MAX][MAX][2];
+int dp(int i,int n,int pos){
+	if(n<0)return -(1<<30);
+	if(i>=v.size()){
+		return ((n%2==1)?-(1<<30):0);
+	}
+	if(memo[i][n][pos])return memo[i][n][pos];
+	int x=(pos?1:-1);
+	if(v[i]=='F'){
+		return memo[i][n][pos]=max(dp(i+1,n-1,pos^1),dp(i+1,n,pos)+x);
+	}
+	else {
+		return memo[i][n][pos]=max(dp(i+1,n,pos^1),dp(i+1,n-1,pos)+x);
+	}
+}
 	int main (){
-		int n,i,j,k,ts=0,fs=0;
-		string v;
-		cin>>n;
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		int n,u;
+		cin>>u;
 		cin>>v;
-		cin>>k;
-		for(i=0;i<v.size();i++){
-			if(v[i]=='T')ts++;
-			if(v[i]=='F')fs++;
-		}
-		if(k==ts){
-		cout << v.size();
-		return 0;
-		}
-		else if(k<ts){
-			//Aquí dentro todo lo del recorrido :P	
-		}
-		k-=ts;
-		if(k%2!=0){
-			cout <<ts+fs-1;
-		}
-		else cout <<v.size();
+		cin>>n;
+		cout << max(dp(0,n,0),dp(0,n,1))<<"\n";	
 	}
