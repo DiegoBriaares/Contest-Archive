b9b114ce40a837630db1e7551adc6d5e66c7b554
Diego González Hernández <diego.gonzalez@cimat.mx>
2017-02-26 04:40:31 -0600

    D

---
+++ Codeforces/402/D.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX 300002
+int n,r;
+string aux;
+string a,b;
+int v[MAX];
+	int main (){
+		ios_base::sync_with_stdio(0);
+		cin.tie(0);
+		cin>>a>>b;
+		n=a.size();
+		int idx=0;
+		for(int i=0;i<n;i++){
+			cin>>v[i];
+			v[i]--;
+		}
+		int in=0,fin=n-1;
+		while(in<=fin){
+			int mid=(in+fin)/2;
+			aux=a;
+			for(int i=0;i<=mid;i++){
+				aux[v[i]]='.';
+			}
+			idx=0;
+			for(int i=0;idx<b.size()&&i<n;i++){
+				if(aux[i]==b[idx])idx++;
+			}
+			if(idx==b.size()){
+				r=max(r,mid);
+				in=mid+1;
+			}
+			else fin=mid-1;
+		}
+		cout << r+1<<"\n";
+	}
