d9d3ddb2780b7a59c2f9b5fc38e8c57d9e0567b1
Diego González Hernández <diego.gonzalez@cimat.mx>
2017-12-31 01:48:47 -0600

    OMEGAUP

--- Solutions/Torres.cpp
+++ Solutions/Torres.cpp
@@ -1,31 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define MAX 5002
-int v[MAX],n;
-map<pair<int,int>,int>memo;
-int dp(int i,int p){
-	if(i==n)return 0;
-	pair<int,int>uso=make_pair(i,p);
-	if(memo[uso])return memo[uso];
-	if(v[i]<p){
-		int uso2=v[i],j,r=0;
-		for(j=i+1;uso2<p&&j<n;j++){
-			uso2+=v[j];
-			r++;
-		}
-		if(j==n&&uso2<p)return memo[uso]=1+dp(i+1,p+v[i]);
-		else if(uso2>=p){
-			return memo[uso]=min(r+dp(j,uso2),1+dp(i+1,p+v[i]));
-		}
+#define ll int
+#define INF 999999999
+int n;
+ll v[MAX];
+ll dp[MAX][MAX];
+ll suma[MAX],sum[MAX][MAX];
+void prec(){
+    for(int i=1; i<=n; i++){
+        suma[i]=suma[i-1]+v[i];
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<=n; j++){
+            sum[i][j]=suma[j]-suma[i-1]; 
+        }    
+    }    
+}
+int bin(int in,int fin,int a,ll s){
+    int mid=(in+fin)/2;
+    if(in==fin){
+    	if(sum[a][in]>=s)return in;
+    	return -1;
 	}
-		return memo[uso]=dp(i+1,v[i]);
+	if(sum[a][mid]>=s)return bin(in,mid,a,s);
+	return bin(mid+1,fin,a,s);
 }
 	int main (){
 		ios_base::sync_with_stdio(0);
 		cin.tie(0);
 		cin>>n;
-		for(int i=0;i<n;i++){
+		for(int i=1;i<=n;i++){
 			cin>>v[i];
 		}
-		cout << dp(0,0)<<"\n";
+		prec();
+		for(int i=n;i>=1;i--){
+			ll a,b;
+			for(int j=n;j>=i;j--){
+				a=b=(INF);
+				a=dp[i][j+1]+1;
+				if(j==n)a--;
+				if(j<n){
+					if(sum[j+1][n]>=sum[i][j]){
+						int pos=bin(j+1,n,j+1,sum[i][j]);
+						if(pos==-1)continue;
+						b=dp[j+1][pos]+(pos-(j+1));
+					}
+				}
+				dp[i][j]=min(a,b);
+			}
+		}
+		cout << dp[1][1]<<"\n";
 	}
