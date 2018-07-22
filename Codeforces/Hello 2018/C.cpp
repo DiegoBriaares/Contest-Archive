3edad6980756591cf002563c0559d41b8a554d77
Diego González Hernández <diego.gonzalez@cimat.mx>
2018-07-21 23:20:25 -0500

    C

--- Codeforces/Hello 2018/C.cpp
+++ Codeforces/Hello 2018/C.cpp
@@ -1,32 +1,46 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define MAX 32
-#define ll unsigned long long
-#define INF 999999999999999999
-ll n,k,res,l;
-ll pot[MAX];
-ll v[MAX];
-	int main (){
-		ios_base::sync_with_stdio(0);
-		cin.tie(0);
-		cin>>n>>k;
-		pot[0]=1LL;
-		for(int i=1;i<MAX;i++)pot[i]=pot[i-1]*2LL;
-		v[0]=INF;
-		for(int i=0;i<n;i++){
-			cin>>v[i];
-		}
-		for(int i=1;i<32;i++){
-			if(2*v[i-1]<v[i]||v[i]==0)v[i]=2*v[i-1];
-		}
-		for(int i=0;i<32;i++){
-			if(i==0){
-				res+=v[i]*(k%2);
-			}
-			else {
-				if(res>v[i])res=v[i];
-				if((k&(1LL<<i)))res+=v[i];
-			}
-		}
-		cout << res<<"\n";
-	}
+
+typedef long long ll;
+const ll MaxN = 310002;
+const ll MaxLog = 31;
+const ll INF = 1000000000000000000;
+
+ll n, L, cost, ans;
+ll c[MaxN], minCost[MaxN];
+
+int main () {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> L;
+  for (int i = 0; i < n; i++) {
+    cin >> c[i];
+  }
+  minCost[0] = c[0];
+  for (int i = 1; i < MaxLog; i++) {
+    if (i < n) {
+      minCost[i] = min(c[i], minCost[i - 1] * 2LL);
+    }
+    else {
+      minCost[i] = minCost[i - 1] * 2LL;
+    }
+  }
+  ll d = 0;
+  ans = INF;
+  for (int i = 0; i < MaxLog; i++) {
+    if ((L & (1LL << i))) {
+      d = i;
+    }
+  }
+  for (int i = MaxLog - 1; i >= 0; i--) {
+    if ((L & (1LL << i))) {
+      cost += minCost[i];
+    }
+    else {
+      ans = min(ans, minCost[i] + cost);
+    }
+  }
+  ans = min(ans, cost);
+  cout << ans << "\n";
+}
