34d82d61833ed14f1e313e0d853fe6f15fbb4f61
Diego González Hernández <diego.gonzalez@cimat.mx>
2018-07-21 23:21:34 -0500

    A

--- Codeforces/Hello 2018/A.cpp
+++ Codeforces/Hello 2018/A.cpp
@@ -1,18 +1,26 @@
 #include <bits/stdc++.h>
+
 using namespace std;
-#define MAX 110002
-#define ll long long
-ll n,k;
-ll v[MAX];
-	int main (){
-		ios_base::sync_with_stdio(0);
-		cin.tie(0);
-		cin>>n>>k;
-		ll pot=1LL,p=0;
-		while(p<n&&pot<=k){
-			pot*=2LL;
-			p++;
-		}
-		if(pot>k)cout << k<<"\n";
-		else cout << (k%pot)<<"\n";
-	}
+
+typedef long long ll;
+const ll MaxN = 310002;
+
+ll n, m;
+ll v[MaxN];
+
+int main () {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cin >> n >> m;
+  ll pot = 1;
+  while (n > 0 && pot < m) {
+    pot *= 2LL;
+    n--;
+  }
+  if (n) {
+    cout << m << "\n";
+  }
+  else {
+    cout << (m - (m / pot) * pot) << "\n";
+  }
+}
