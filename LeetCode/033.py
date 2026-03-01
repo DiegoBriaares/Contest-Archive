69ec39fc05ee838593562b14eb024c123d2d143f
Briaares - Diego González Hernández <diegobriaares@gmail.com>
2026-02-28 18:39:00 -0600

    33. Search in Rotated Sorted Array
    Refactored binary search implementation for clarity and efficiency.

--- LeetCode/033.py
+++ LeetCode/033.py
@@ -1,3 +1,40 @@
+# Short Solution
+class Solution:
+
+    def BS (self, nums, l, r, check):
+        idx = 0
+        while l <= r:
+            mid = (l + r) >> 1
+            if check(mid):
+                l = mid + 1
+            else:
+                idx = mid
+                r = mid - 1
+                
+        return idx
+
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        p = -1
+        if nums[n - 1] < nums[0]:
+            p = self.BS(nums, 0, n - 1, (lambda mid: nums[n - 1] < nums[mid]))
+
+        idx = 0
+        if (p == -1):
+            idx = self.BS(nums, 0, n - 1, (lambda mid: nums[mid] < target))
+        
+        else:
+            idx = max(self.BS(nums, 0, p - 1, (lambda mid: nums[mid] < target)), 
+            self.BS(nums, p, n - 1, (lambda mid: nums[mid] < target)))
+
+        if nums[idx] == target:
+            return idx
+        else:
+            return -1
+
+'''
+Primary Solution
 class Solution:
 
     def BS (self, nums, l, r, v):
@@ -36,4 +73,4 @@ class Solution:
             self.BS(nums, p, n - 1, target))
 
         return idx
-
+'''
