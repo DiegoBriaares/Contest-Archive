0df83b021d5fa0c22cf7ba3b28c833d8aa0b404e
Briaares - Diego González Hernández <diegobriaares@gmail.com>
2026-02-28 18:26:15 -0600

    33. Search in Rotated Sorted Array

---
+++ LeetCode/33.py
@@ -0,0 +1,39 @@
+class Solution:
+
+    def BS (self, nums, l, r, v):
+        idx = -1
+        while l <= r:
+            mid = (l + r) >> 1
+            if nums[mid] < v:
+                l = mid + 1
+            else:
+                if nums[mid] == v:
+                    idx = mid
+                r = mid - 1
+
+        return idx
+
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        p = -1
+        if nums[n - 1] < nums[0]:
+            l, r = 0, n - 1
+            while l <= r:
+                mid = (l + r) >> 1
+                if nums[n - 1] < nums[mid]:
+                    l = mid + 1
+                else:
+                    p = mid
+                    r = mid - 1
+
+        idx = -1
+        if (p == -1):
+            idx = self.BS(nums, 0, n - 1, target)
+        
+        else:
+            idx = max(self.BS(nums, 0, p - 1, target), 
+            self.BS(nums, p, n - 1, target))
+
+        return idx
+
